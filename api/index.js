3962f4dbaa43485f533d155da54b4764aab5cf3c
Arijeet Das Chowdhury <116735842+Arijeet04@users.noreply.github.com>
2024-02-14 11:46:40 +0530

    Testing the Commit

--- api/index.js
+++ api/index.js
@@ -23,7 +23,7 @@ app.listen(3000,() =>{
 });
 
 app.use('/api/user' , userRouter);
-app.use('/api/auth', authRouter);
+app.use('/api/auth', authRouter); 
 
 
 // Created a MIDDLEWARE for Handling all the Errors
